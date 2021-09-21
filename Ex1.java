09eb5e3c7602a4a7a2582797b34176a03841c1e9
kwconvergence <konang1@naver.com>
2021-09-21 16:18:04 +0900

    Second commit

--- Ex1.java
+++ Ex1.java
@@ -3,7 +3,7 @@ public class Ex1 {
         System.out.println("안녕하세요!");
         System.out.print("저는");
         System.out.println(" 홍길동입니다.");
-        System.out.println("");
+        System.out.print("\n");
         System.out.println("반갑습니다.");
     }
 }
